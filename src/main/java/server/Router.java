57673776fe56596f71f8758e3854b14f108714a5
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-06-10 12:17:16 -0500

    Extract status codes to constants

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -4,15 +4,17 @@ import main.java.server.request.Request;
 
 import java.util.HashMap;
 
+import static main.java.server.StatusCodes.*;
+
 public class Router {
 
     public String route(Request request) {
         HashMap<String, String> requestHeaders = request.getHeaders();
         String path = requestHeaders.get("path");
         if (path.equals("/simple_get")) {
-            return "HTTP/1.1 200 OK\r\n";
+            return OK;
         } else {
-            return "HTTP/1.1 404 Not Found\r\n";
+            return NOT_FOUND;
         }
     }
 
