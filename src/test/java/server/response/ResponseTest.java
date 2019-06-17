ecb15441f6266f0f759a28d115c5ae941347485b
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-06-17 16:53:33 -0500

    Add test for response header creation

--- src/test/java/server/response/ResponseTest.java
+++ src/test/java/server/response/ResponseTest.java
@@ -1,7 +1,9 @@
-package test.java.server;
+package test.java.server.response;
 
 import main.java.server.response.Response;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static main.java.server.HTTPMessageComponents.HTTPSyntax.*;
 import static org.junit.Assert.assertEquals;
@@ -15,5 +17,16 @@ public class ResponseTest {
         Response response = new Response(VERSION + SP + OK + CRLF);
 
         assertEquals("HTTP/1.1 200 OK\r\n", response.getStatusLine());
+    }
+
+    @Test
+    public void responseCreatedWithStatusLineAndHeaders() {
+        String statusLine = VERSION + SP + OK + CRLF;
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Allow:", "GET, HEAD, OPTIONS");
+        Response response = new Response(statusLine, headers);
+
+        assertEquals("HTTP/1.1 200 OK\r\n", response.getStatusLine());
+        assertEquals(headers, response.getHeaders());
     }
 }
