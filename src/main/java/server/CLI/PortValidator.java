95491d4680940cfd6488d687456c4c9a92d0dfc7
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-06-19 09:48:24 -0500

    Define CL args and validation
    Args defines --port as a required argument when starting the program. It
    is set to accept an integer between 1024 and 49151 inclusive so as not
    to interfere with system ports (under 1024) and dynamic ports (over
    49151). If no argument is provided it will display usage to the user.
    If a non integer is given it will exit and display a prompt to the user.
    Users can also pass the --help argument to get the usage.

---
+++ src/main/java/server/CLI/PortValidator.java
@@ -0,0 +1,24 @@
+package main.java.server.CLI;
+
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.ParameterException;
+import main.java.server.ServerLogger;
+
+import java.util.logging.Level;
+
+public class PortValidator implements IParameterValidator {
+
+    @Override
+    public void validate(String name, String value) throws ParameterException {
+        int n = 0;
+
+        try {
+            n = Integer.parseInt(value);
+        } catch (Exception e) {
+            ServerLogger.serverLogger.log(Level.FINE, "Error: " + e);
+        }
+
+        if (n < 1024 || n > 49151)
+            throw new ParameterException("Parameter " + name + " should be a number between 1024 and 49151 (found " + value + ")");
+        }
+}
