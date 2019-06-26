4d23fc8d3396111d72aaf8eacd1e37a960d6424f
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-06-26 11:54:45 -0500

    Clarify route/path naming conventions
    A route now refers to the entire journey that a request could follow, as
    in a route is a path + allowed methods + related handler. Paths that
    were also labelled routes are now appropriately referred to as paths.

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -1,6 +1,6 @@
 package main.java.server;
 
-import main.java.application.Handler;
+import main.java.server.response.Handler;
 
 import java.util.HashMap;
 import java.util.TreeMap;
@@ -14,24 +14,24 @@ public class Router {
 
     public TreeMap<String, HashMap<String, Handler>> routes() { return routes; }
 
-    public void get(String route, Handler handler) { addToRoutes(GET, route, handler); }
+    public void get(String path, Handler handler) { addToRoutes(GET, path, handler); }
 
-    public void head(String route, Handler handler) { addToRoutes(HEAD, route, handler); }
+    public void head(String path, Handler handler) { addToRoutes(HEAD, path, handler); }
 
-    public void options(String route, Handler handler) {
-        addToRoutes(OPTIONS, route, handler);
+    public void options(String path, Handler handler) {
+        addToRoutes(OPTIONS, path, handler);
     }
 
-    public void put(String route, Handler handler) {
-        addToRoutes(PUT, route, handler);
+    public void put(String path, Handler handler) {
+        addToRoutes(PUT, path, handler);
     }
 
-    public void post(String route, Handler handler) {
-        addToRoutes(POST, route, handler);
+    public void post(String path, Handler handler) {
+        addToRoutes(POST, path, handler);
     }
 
-    private void addToRoutes(String method, String route, Handler handler) {
-        routes.putIfAbsent(route, new HashMap<>());
-        routes.get(route).put(method, handler);
+    private void addToRoutes(String method, String path, Handler handler) {
+        routes.putIfAbsent(path, new HashMap<>());
+        routes.get(path).put(method, handler);
     }
 }
