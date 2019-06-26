1d58fa5c649fd88ed5734aaf786c050412355c1e
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-06-26 11:49:59 -0500

    Add redirect location

--- src/main/java/application/Handlers.java
+++ src/main/java/application/Handlers.java
@@ -1,11 +1,12 @@
 package main.java.application;
 
 import main.java.server.request.Request;
+import main.java.server.response.Handler;
 
 class Handlers {
-    static final Handler SimpleGet = (Request request) -> HandlerHelper.assembleResponse(request, "Welcome to /simple_get");
+    static final Handler SimpleGet = (Request request) -> HandlerHelper.assembleResponse(request, "");
     static final Handler GetWithBody = (Request request) -> HandlerHelper.assembleResponse(request, "");
     static final Handler SimpleOptions = (Request request) -> HandlerHelper.assembleResponse(request, "");
-    static final Handler EchoBody = (Request request) -> HandlerHelper.assembleResponse(request, "Request body");
-
+    static final Handler EchoBody = (Request request) -> HandlerHelper.assembleResponse(request, request.getBody());
+    static final Handler Redirect = (Request request) -> HandlerHelper.redirect(request, "http://127.0.0.1:5000/simple_get");
 }
