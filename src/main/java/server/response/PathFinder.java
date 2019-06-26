3b6b551e94681bd6288ba71f414371fdaad70de9
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-06-26 11:58:45 -0500

    Refactor naming and duplication

--- src/main/java/server/response/PathFinder.java
+++ src/main/java/server/response/PathFinder.java
@@ -3,37 +3,44 @@ package main.java.server.response;
 import main.java.application.App;
 import main.java.server.request.Request;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class PathFinder {
     private final App app;
-    private Request request;
+    private static ResponseBuilder builder = new ResponseBuilder();
 
-    public PathFinder(Request request, App app) {
-        this.request = request;
+    public PathFinder(App app) {
         this.app = app;
     }
 
-    public Response selectHandler() {
+    public Response getResponse(Request request) {
 
-         if (!pathExists()) {
-             ResponseBuilder builder = new ResponseBuilder();
-             return builder.notFound(request, app.routes());
+         if (!pathExists(request)) {
+
+             return builder.notFound(request);
          }
 
-        if (!methodAllowed()) {
-            ResponseBuilder builder = new ResponseBuilder();
-            return builder.notAllowed(request, app.routes());
+        if (!methodAllowed(request)) {
+            HashMap<String, Handler> pathMethods = app.routes().get(request.getRequestPath());
+
+            return builder.notAllowed(request, pathMethods);
         }
 
-        return app.routes().get(request.getRequestPath()).get(request.getRequestMethod()).generateResponse(request);
+        TreeMap<String, HashMap<String, Handler>> routes = app.routes();
+        HashMap<String, Handler> handlerCollection = routes.get(request.getRequestPath());
+        Handler routeHandler = handlerCollection.get(request.getRequestMethod());
+
+        return routeHandler.generateResponse(request);
     }
 
-    private boolean pathExists() {
+    private boolean pathExists(Request request) {
         return app.routes()
                     .keySet()
                     .contains(request.getRequestPath());
     }
 
-    private boolean methodAllowed() {
+    private boolean methodAllowed(Request request) {
         return app.routes()
                     .get(request.getRequestPath())
                     .containsKey(request.getRequestMethod());
--- src/test/java/server/response/PathFinderTest.java
+++ src/test/java/server/response/PathFinderTest.java
@@ -21,8 +21,8 @@ public class PathFinderTest {
     @Test
     public void findsMatchingRouteForSimpleGetRequestAndGeneratesResponse() {
         Request simpleGetRequest = new Request(new StatusLine("GET", "/simple_get", "HTTP1.1"));
-        pathFinder = new PathFinder(simpleGetRequest, app);
-        Response simpleGetResponse = pathFinder.selectHandler();
+        pathFinder = new PathFinder(app);
+        Response simpleGetResponse = pathFinder.getResponse(simpleGetRequest);
 
         assertEquals("HTTP/1.1 200 OK\r\n", simpleGetResponse.getStatusLine());
         }
@@ -30,8 +30,8 @@ public class PathFinderTest {
     @Test
     public void returnsNotFoundResponseForUnknownRoute() {
         Request unknownRequest = new Request(new StatusLine("GET", "/WHO_GOES_THERE", "HTTP1.1"));
-        pathFinder = new PathFinder(unknownRequest, app);
-        Response unknownResponse = pathFinder.selectHandler();
+        pathFinder = new PathFinder(app);
+        Response unknownResponse = pathFinder.getResponse(unknownRequest);
 
         assertEquals("HTTP/1.1 404 Not Found\r\n", unknownResponse.getStatusLine());
     }
@@ -39,10 +39,10 @@ public class PathFinderTest {
     @Test
     public void returnsMethodNotAllowedForRequestWithForbiddenMethod() {
         Request wrongMethodRequest = new Request(new StatusLine("PUT", "/simple_get", "HTTP1.1"));
-        pathFinder = new PathFinder(wrongMethodRequest, app);
-        Response unknownResponse = pathFinder.selectHandler();
+        pathFinder = new PathFinder(app);
+        Response wrongMethodResponse = pathFinder.getResponse(wrongMethodRequest);
 
-        assertEquals("HTTP/1.1 405 Method Not Allowed\r\n", unknownResponse.getStatusLine());
+        assertEquals("HTTP/1.1 405 Method Not Allowed\r\n", wrongMethodResponse.getStatusLine());
     }
 }
 
